02e147e0a0d5726f53e4b4ab8f832a18819b864b
karlk <karlk@DESKTOP-VHFKDKV.lan>
2016-03-18 04:24:54 +0200

    Cleaned up JS.

--- WebContent/static/default.js
+++ WebContent/static/default.js
@@ -11,37 +11,21 @@ function swapCurrencies(){
 $(function() {
 	$("#datepicker").datepicker({
 		dateFormat : "dd.mm.yy",
-		//minDate: 
-		//maxDate : new Date(2010, 11, 30),
 		maxDate : -1, // YESTERDAY
 		changeMonth : true,
 		changeYear : true,
 		
-		beforeShow: function(el){
-	        // set the current value before showing the widget
+		beforeShow: function(el){ // set the current value before showing the widget
 	        $(this).data('previous', $(el).val() );
 	    },
-		onSelect: function(newDateText) {
-			// compare the new value to the previous one
-	        if( $(this).data('previous') != newDateText ){
-	            // do whatever has to be done, e.g. log it to console
-	            console.log( 'datePicker changed to: ' + newDateText );
+		onSelect: function(newDateText) { // compare the new value to the previous one
+	        if( $(this).data('previous') != newDateText ){ // do whatever has to be done, e.g. log it to console: console.log('datePicker changed to: ' + newDateText);
 	            $('#datePickerForm').submit();
 	        }
-			//console.log("Selected date: " + newDateText + "; input's current value: " + this.value); // both same eg 05.03.2016
-			// TODO upon change GET ALL DISPLAYED CURRENCIES!, GET?
 		}
 	});
 	var dateYesterday = new Date();
 	dateYesterday.setDate(dateYesterday.getDate() - 1); // YESTERDAY
-	//$("#datepicker").datepicker('setDate', dateYesterday); // DEFAULT DATE SET TO YESTERDAY //new Date(2010, 11, 30));
-
-	//$("#datepicker").datepicker();
-	//$("#datepicker").datepicker("option", "dateFormat", "dd.mm.yy");
-
-	//$( "#datepicker" ).datepicker({ minDate: -20, maxDate: "+1M +10D" });
-
-	//var date = $('#datepicker').datepicker({ dateFormat: 'dd-mm-yy' }).val();
 });
 
 $(document).on("click","#swapCurrenciesBtn",function(event){
@@ -64,69 +48,31 @@ $(document).on("change","#selectInputCurrency",function(event){
 $(document).on("submit","#someForm",function(event) {
 	console.log("submitting the ajax post form");
 	var $form = $(this);
-	/*
-	$.get("Something", function(responseXml) {                // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response XML...
-	    $("#somediv").html($(responseXml).find("data").html()); // Parse XML, find <data> element and append its HTML to HTML DOM element with ID "somediv".
-	});
-	 */
-
-	 //  + "&par1=1&par2=2&par3=232"
+	// Adding selectedD (date) and lang (language) as extra params to serialized form!
 	$.post($form.attr("action"), $form.serialize() +"&selectedD="+$("#datepicker").datepicker().val()+"&lang="+$("#languageSelect").val(), function(responseJson) { // responseText responseJson responseXml
 		console.log("responseJson is: " + responseJson);
-
-		//$("#somediv").html($(responseXml).find("data").html());
-
-		/*
-		console.log("responseText is: " + responseText);
-		$("#outResult").val(responseText); // text
-		 */
-
-		// POPULATE SELECT WITH RESULTS
-		//var $select = $("#someselect"); // Locate HTML DOM element with ID "someselect".
-		//$select.find("option").remove(); // Find all child elements with tag name "option" and remove them (just to prevent duplicate options when button is pressed again).
-		//$.each(responseJson, function(key, value) { // Iterate over the JSON object.
-		//	$("<option>").val(key).text(value).appendTo($select); // Create HTML <option> element, set its value with currently iterated key and its text content with currently iterated item and finally append it to the <select>.
-		//});
-		//var $table = $("<table>").appendTo($("#somediv")); // Create HTML <table> element and append it to HTML DOM element with ID "somediv".
-
 		// EMPTY BOTH RESULTS AND ERRORS HOLDERS:
 		$("#errorsTableContainer").html("");
 		$('#resultsTableContainer').html("");
-
 		// I GOT ERRORS:
-		if(typeof responseJson[0] === 'string'){
-			//console.log("responseJson[0]: " + responseJson[0] +" is string, therefore probably error messages");
+		if(typeof responseJson[0] === 'string'){ // responseJson[0] is string, therefore responseJson has error messages.
 			var $ul = $("<ul>");
 			$("<li id='errorsList'>").appendTo($ul).text("<fmt:message key='label.error' />:"); // Errors:
 			$.each(responseJson, function(index, item) { // Iterate over the JSON array.
-				console.log("2index " + index + " item " + item);
-				$("<li>").text(item).appendTo($ul); // error.inputMoneyEmpty // "<fmt:message key='currency.AED' />"
-				//$("<li>").text(item).appendTo($ul); // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+				$("<li>").text(item).appendTo($ul);
 			});
 			$("#errorsTableContainer").html($ul);
 		}else{
-			// I GOT RESULTS:
-			var $table = $("<table>"); //.appendTo($("#somediv"));
+		// NO ERRORS:
+			var $table = $("<table>");
 			$("<tr>").appendTo($table).append($("<th>").text("Bank:")).append($("<th>").text("Result:"));
-
 			$.each(responseJson, function(index, result) { // Iterate over the JSON array.
-				//console.log("1index " + index + " item " + result);
 				$("<tr>").appendTo($table).append(
 						$("<td>").text(result._bankName)).append(
 								$("<td>").text(result._resultValue));
-				//$("<tr>").($table)                     
-
-				//$("<tr>").appendTo($table) // Create HTML <tr> element, set its text content with currently iterated item and append it to the <table>.
-				//	.append($("<td>").text("Bank: " + result._bankName))        // Create HTML <td> element, set its text content with id of currently iterated product and append it to the <tr>.
-				//    .append($("<td>").text("Result: " + result._resultValue))
 			});
 			$('#resultsTableContainer').html($table);
 		}
-		//$.get("Something", function(responseXml) {                // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response XML...
-		//    $("#somediv").html($(responseXml).find("data").html()); // Parse XML, find <data> element and append its HTML to HTML DOM element with ID "somediv".
-		//});
-		//$("#somediv").html($(responseXml).find("data").html());
-
 	});
 	event.preventDefault(); // Important! Prevents submitting the form.
 });
@@ -167,6 +113,130 @@ $(document).on("submit","#someForm",function(event) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$(function() {
+//	$("#datepicker").datepicker({
+//		dateFormat : "dd.mm.yy",
+//		//minDate: 
+//		//maxDate : new Date(2010, 11, 30),
+//		maxDate : -1, // YESTERDAY
+//		changeMonth : true,
+//		changeYear : true,
+//		
+//		beforeShow: function(el){
+//	        // set the current value before showing the widget
+//	        $(this).data('previous', $(el).val() );
+//	    },
+//		onSelect: function(newDateText) {
+//			// compare the new value to the previous one
+//	        if( $(this).data('previous') != newDateText ){
+//	            // do whatever has to be done, e.g. log it to console
+//	            console.log( 'datePicker changed to: ' + newDateText );
+//	            $('#datePickerForm').submit();
+//	        }
+//			//console.log("Selected date: " + newDateText + "; input's current value: " + this.value); // both same eg 05.03.2016
+//			// upon change GET ALL DISPLAYED CURRENCIES!, GET?
+//		}
+//	});
+//	var dateYesterday = new Date();
+//	dateYesterday.setDate(dateYesterday.getDate() - 1); // YESTERDAY
+//	//$("#datepicker").datepicker('setDate', dateYesterday); // DEFAULT DATE SET TO YESTERDAY //new Date(2010, 11, 30));
+//
+//	//$("#datepicker").datepicker();
+//	//$("#datepicker").datepicker("option", "dateFormat", "dd.mm.yy");
+//
+//	//$( "#datepicker" ).datepicker({ minDate: -20, maxDate: "+1M +10D" });
+//
+//	//var date = $('#datepicker').datepicker({ dateFormat: 'dd-mm-yy' }).val();
+//});
+
+////Ajaxifying an existing form:
+//$(document).on("submit","#someForm",function(event) {
+//	console.log("submitting the ajax post form");
+//	var $form = $(this);
+//	/*
+//	$.get("Something", function(responseXml) {                // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response XML...
+//	    $("#somediv").html($(responseXml).find("data").html()); // Parse XML, find <data> element and append its HTML to HTML DOM element with ID "somediv".
+//	});
+//	 */
+//
+//	 //  + "&par1=1&par2=2&par3=232"
+//	$.post($form.attr("action"), $form.serialize() +"&selectedD="+$("#datepicker").datepicker().val()+"&lang="+$("#languageSelect").val(), function(responseJson) { // responseText responseJson responseXml
+//		console.log("responseJson is: " + responseJson);
+//
+//		//$("#somediv").html($(responseXml).find("data").html());
+//
+//		/*
+//		console.log("responseText is: " + responseText);
+//		$("#outResult").val(responseText); // text
+//		 */
+//
+//		// POPULATE SELECT WITH RESULTS
+//		//var $select = $("#someselect"); // Locate HTML DOM element with ID "someselect".
+//		//$select.find("option").remove(); // Find all child elements with tag name "option" and remove them (just to prevent duplicate options when button is pressed again).
+//		//$.each(responseJson, function(key, value) { // Iterate over the JSON object.
+//		//	$("<option>").val(key).text(value).appendTo($select); // Create HTML <option> element, set its value with currently iterated key and its text content with currently iterated item and finally append it to the <select>.
+//		//});
+//		//var $table = $("<table>").appendTo($("#somediv")); // Create HTML <table> element and append it to HTML DOM element with ID "somediv".
+//
+//		// EMPTY BOTH RESULTS AND ERRORS HOLDERS:
+//		$("#errorsTableContainer").html("");
+//		$('#resultsTableContainer').html("");
+//
+//		// I GOT ERRORS:
+//		if(typeof responseJson[0] === 'string'){
+//			//console.log("responseJson[0]: " + responseJson[0] +" is string, therefore probably error messages");
+//			var $ul = $("<ul>");
+//			$("<li id='errorsList'>").appendTo($ul).text("<fmt:message key='label.error' />:"); // Errors:
+//			$.each(responseJson, function(index, item) { // Iterate over the JSON array.
+//				console.log("2index " + index + " item " + item);
+//				$("<li>").text(item).appendTo($ul); // error.inputMoneyEmpty // "<fmt:message key='currency.AED' />"
+//				//$("<li>").text(item).appendTo($ul); // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
+//			});
+//			$("#errorsTableContainer").html($ul);
+//		}else{
+//			// I GOT RESULTS:
+//			var $table = $("<table>"); //.appendTo($("#somediv"));
+//			$("<tr>").appendTo($table).append($("<th>").text("Bank:")).append($("<th>").text("Result:"));
+//
+//			$.each(responseJson, function(index, result) { // Iterate over the JSON array.
+//				//console.log("1index " + index + " item " + result);
+//				$("<tr>").appendTo($table).append(
+//						$("<td>").text(result._bankName)).append(
+//								$("<td>").text(result._resultValue));
+//				//$("<tr>").($table)                     
+//
+//				//$("<tr>").appendTo($table) // Create HTML <tr> element, set its text content with currently iterated item and append it to the <table>.
+//				//	.append($("<td>").text("Bank: " + result._bankName))        // Create HTML <td> element, set its text content with id of currently iterated product and append it to the <tr>.
+//				//    .append($("<td>").text("Result: " + result._resultValue))
+//			});
+//			$('#resultsTableContainer').html($table);
+//		}
+//		//$.get("Something", function(responseXml) {                // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response XML...
+//		//    $("#somediv").html($(responseXml).find("data").html()); // Parse XML, find <data> element and append its HTML to HTML DOM element with ID "somediv".
+//		//});
+//		//$("#somediv").html($(responseXml).find("data").html());
+//
+//	});
+//	event.preventDefault(); // Important! Prevents submitting the form.
+//});
 
 //console.log("Default.js loaded");
 
